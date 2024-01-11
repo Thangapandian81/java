37386da6c58e47ceaf79c31bc124cb6428588f4b
Thangapandian81 <22us44@anjaconline.org>
2024-01-11 07:20:39 +0000

    First commit

---
+++ Array_demo.java
@@ -0,0 +1,10 @@
+import java.io.*;
+class Array_demo{
+public static void main(String args[])
+{
+int[] numbers={1,2,3,4,5};
+int len=numbers.length;
+System.out.println("The length of the array is:"+len);
+
+}
+}
