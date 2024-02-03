9863920fa3c1a31b83d84704780de1fbcb7c448c
Thangapandian81 <22us44@anjaconline.org>
2024-02-03 11:20:02 +0000

    First commit

--- Stringpro.java
+++ Stringpro.java
@@ -7,7 +7,7 @@ class Stringpro
 
         // Altermethod
         String join="";
-        
+        String space=" ";
         for(int i=0;i<words.length;i++)
         {   
             if(i>0)
@@ -17,7 +17,12 @@ class Stringpro
             join=join.concat(words[i]);
             System.out.println(join);
         }
-        
+         for(int i=0;i<words.length;i++)
+        {   
+           
+            join=join.concat(space).concat(words[i]);
+            System.out.println(join.trim());
+        }
         
     }
 }
